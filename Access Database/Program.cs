64e6a9c90f0a35fe1a29d6ad67aba88c06e4085e
MdotKvist <Kvist339@gmail.com>
2024-03-21 21:33:26 +0100

    Initial commit

---
+++ Access Database/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Common;
+using Npgsql;
+
+// Remember to insert your database credentials in this string
+var connectionString = "INSERT YOUR DATABASE CREDENTIALS HERE";
+await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+
+
+
+//Read
+await using (var cmd = dataSource.CreateCommand("SELECT * FROM test"))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+    while (reader.Read())
+    {
+        Console.WriteLine(reader.GetValue(0));
+    }
+}
+
+
+//Write
+await using (var cmd = dataSource.CreateCommand("INSERT INTO test (text) VALUES ($1)"))
+{
+    cmd.Parameters.AddWithValue(Console.ReadLine());
+    await cmd.ExecuteNonQueryAsync();
+}
